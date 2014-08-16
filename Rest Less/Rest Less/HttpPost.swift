2d15a53fc70686814c48fc6bc737fc585b5e154e
Dylan Krause <dykrause@gmail.com>
2014-08-16 18:39:09 -0400

    Setup HTTP_Post File

---
+++ Rest Less/Rest Less/HttpPost.swift
@@ -0,0 +1,38 @@
+//
+//  HttpPost.swift
+//  Rest Less
+//
+//  Created by Dylan Krause on 8/16/14.
+//  Copyright (c) 2014 newts. All rights reserved.
+//
+
+import Foundation
+func HTTPostJSON(url: String,
+    jsonObj: AnyObject)
+{
+    
+    var request = NSMutableURLRequest(URL: NSURL(string: url))
+    var session = NSURLSession.sharedSession()
+    
+    var jsonError:NSError?
+    request.HTTPMethod = "POST"
+    request.HTTPBody = NSJSONSerialization.dataWithJSONObject( jsonObj, options: nil, error: &jsonError)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    var subTask = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+        
+        var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+        
+        var jsonRError: NSError?
+        var json_response = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &jsonRError) as? NSDictionary
+        println(jsonRError)
+        jsonRError?
+        if jsonRError? != nil {
+            println(jsonRError!.localizedDescription)
+        }
+        else {
+            //json_response
+        }
+    })
+    subTask.resume()
+}
--- Rest Less/Rest Less/ViewController.swift
+++ Rest Less/Rest Less/ViewController.swift
@@ -65,6 +65,37 @@ class ViewController: UIViewController {
         displayTimeLabel.text = secondsToDisplay(restTime)
     }
     
+    @IBAction func sendData(sender: AnyObject) {
+        func HTTPostJSON(url: String,
+            jsonObj: AnyObject)
+        {
+            
+            var request = NSMutableURLRequest(URL: NSURL(string: url))
+            var session = NSURLSession.sharedSession()
+            
+            var jsonError:NSError?
+            request.HTTPMethod = "POST"
+            request.HTTPBody = NSJSONSerialization.dataWithJSONObject( jsonObj, options: nil, error: &jsonError)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            var subTask = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+                
+                var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                var jsonRError: NSError?
+                var json_response = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &jsonRError) as? NSDictionary
+                println(jsonRError)
+                jsonRError?
+                if jsonRError? != nil {
+                    println(jsonRError!.localizedDescription)
+                }
+                else {
+                    //json_response
+                }
+            })
+            subTask.resume()
+        }
+    }
     
     func updateTime() {
         
