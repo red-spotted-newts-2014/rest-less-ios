2659fb5ff46ebb3569450bba4caa63657a63f579
Michael <immichaelweiss@gmail.com>
2014-08-17 02:05:56 -0400

    Modularize toSecondsDisplay code by taking it out of ViewController and giving it its own file.

--- Rest Less/Rest Less/ViewController.swift
+++ Rest Less/Rest Less/ViewController.swift
@@ -9,24 +9,7 @@
 
 import UIKit
 
-func secondsToDisplay(var time: NSTimeInterval) -> String{
-    if time <= 0 {
-        return "00:00:00"
-    }
-    let minutes = UInt8(time / 60.0)
-    time -= (NSTimeInterval(minutes) * 60)
 
-    let seconds = UInt8(time)
-    time -= NSTimeInterval(seconds)
-
-    let fraction = UInt8(time * 100)
-    
-    let strMinutes = minutes > 9 ? String(minutes):"0" + String(minutes)
-    let strSeconds = seconds > 9 ? String(seconds):"0" + String(seconds)
-    let strFraction = fraction > 9 ? String(fraction):"0" + String(fraction)
-
-    return "\(strMinutes):\(strSeconds):\(strFraction)"
-}
 
 class ViewController: UIViewController {
     
