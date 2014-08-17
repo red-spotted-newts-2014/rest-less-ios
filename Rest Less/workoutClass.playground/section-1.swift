27a2d8a469dd7d66508748f23237f92482cf1431
Dylan Krause <dykrause@gmail.com>
2014-08-17 04:32:54 -0400

    Setup workout class to be used as model in main app

--- Rest Less/workoutClass.playground/section-1.swift
+++ Rest Less/workoutClass.playground/section-1.swift
@@ -1,5 +1,44 @@
-// Playground - noun: a place where people can play
 
 import UIKit
 
-var str = "Hello, playground"
+
+class WorkoutLogger {
+    
+    var userName: String
+    var workoutName: String
+    var reps = [String]()
+    var times = [Double]()
+    var finalTime = Double()
+    var missedReps: Int {
+        get {
+            
+            return reps.count - times.count
+        }
+    }
+   
+    init(userName:String, workoutName:String) {
+        self.userName = userName
+        self.workoutName = workoutName
+    }
+    
+    func addRestTime(val:Double) -> Double {
+        self.times.append(val)
+        return self.times.last!
+    
+    }
+    func returnDictInfo() ->  [String:String]{
+    
+       return ["userName": self.userName,
+              "workoutName": self.workoutName,
+              "times": "\(self.times)",
+              "finalTime" : "\(self.finalTime)",
+              "missedReps" : "\(self.missedReps)"]
+    }
+
+}
+
+
+
+
+
+
